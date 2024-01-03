bd39824a0563db4bad32f4e121e6727121bdc5c0
orcxus <73879364+andreichernetskii@users.noreply.github.com>
2024-01-03 21:57:54 +0100

    sending login error fixed

--- src/main/java/com/example/finmangerfrontend/exception_handler/CustomExceptionHandler.java
+++ src/main/java/com/example/finmangerfrontend/exception_handler/CustomExceptionHandler.java
@@ -1,12 +1,14 @@
 package com.example.finmangerfrontend.exception_handler;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.naming.AuthenticationException;
 
 @ControllerAdvice
 public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
@@ -16,24 +18,29 @@ public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
 //        return ResponseEntity.status( HttpStatus.FORBIDDEN ).body( message );
 //    }
 
+//    @ExceptionHandler( HttpClientErrorException.class )
+//    public ModelAndView handleHttpClientErrorException( HttpClientErrorException exception ) {
+//        HttpStatus statusCode = ( HttpStatus ) exception.getStatusCode();
+//        ModelAndView modelAndView;
+//
+//        if ( statusCode == HttpStatus.UNAUTHORIZED ) {
+//            modelAndView = new ModelAndView( "redirect:/login?error=true" );
+//            modelAndView.addObject( "errorMessage", exception.getMessage() );
+//        } else {
+//            modelAndView = new ModelAndView( "error" );
+//            modelAndView.addObject( "error", exception.getMessage() );
+//        }
+//
+//        return modelAndView;
+//    }
+
     @ExceptionHandler( HttpClientErrorException.class )
-    public ModelAndView handleHttpClientErrorException( HttpClientErrorException exception ) {
-        HttpStatus statusCode = (HttpStatus) exception.getStatusCode();
-        ModelAndView modelAndView;
-
-        if ( statusCode == HttpStatus.UNAUTHORIZED ) {
-            modelAndView = new ModelAndView( "redirect:/login" );
-            modelAndView.addObject( "error", "Unauthorized" );
-
-        } else if ( statusCode == HttpStatus.resolve( 500 ) ){
-            modelAndView = new ModelAndView( "redirect:/login" );
-            modelAndView.addObject( "error", "Unauthorized" );
-
-        } else {
-            modelAndView = new ModelAndView( "error" );
-            modelAndView.addObject("error", exception.getMessage());
+    public String handleAuthenticationException( HttpClientErrorException exception, Model model ) {
+        HttpStatus statusCode = ( HttpStatus ) exception.getStatusCode();
+        if ( statusCode == HttpStatus.UNAUTHORIZED || statusCode == HttpStatus.FORBIDDEN ) {
+            model.addAttribute( "errorMessage", exception.getMessage() );
+            return "redirect:/login?error=true";
         }
-
-        return modelAndView;
+        return "redirect:/";
     }
 }
