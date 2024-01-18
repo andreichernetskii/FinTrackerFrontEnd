7281df7a3b4aee43c142546859e8556395eb5df0
orcxus <73879364+andreichernetskii@users.noreply.github.com>
2024-01-18 20:19:48 +0100

    added array get + adding new options to select

--- src/main/resources/static/scripts/edit-script.js
+++ src/main/resources/static/scripts/edit-script.js
@@ -1,5 +1,5 @@
 // getting refs of dialog window and form elements
-const dialog = document.getElementById('transaction-dialog');
+const dialog = document.getElementById('dialog');
 const idInput = document.getElementById('id');
 const typeInput = document.getElementById('type');
 const amountInput = document.getElementById('amount');
@@ -10,15 +10,23 @@ const categoryInput = document.getElementById('category');
 // by pressing button dialog window will be shown and filled with data from row
 document.querySelectorAll('.editBtn').forEach((btn) => {
     btn.addEventListener('click', () => {
+
         const id = btn.getAttribute('data-id');
         const type = btn.getAttribute('data-type');
+        const types = btn.getAttribute('data-all-types');
         const amount = btn.getAttribute('data-amount');
         const date = btn.getAttribute('data-date');
         const category = btn.getAttribute('data-category');
 
+        const array = types.replace(/([a-zA-Z]+)/g, '"$1"');
+        const typesArray = JSON.parse(array);
+
         // filling forms with data from row
         if (idInput) idInput.value = id;
-        if (typeInput) typeInput.value = type;
+        if (typeInput) {
+            addNewOptions(typesArray);
+            typeInput.value = type;
+        }
         if (amountInput) amountInput.value = amount;
         if (dateInput) dateInput.value = date;
         if (categoryInput) categoryInput.value = category;
@@ -34,6 +42,7 @@ document.querySelectorAll('.editBtn').forEach((btn) => {
 document.getElementById('cancelDialogBtn').addEventListener('click', (event) => {
     if (dialog) {
         dialog.classList.add('hidden');
+        typeInput.innerHTML = "";
     }
 });
 
@@ -45,4 +54,15 @@ function centerDialog() {
     const topPosition = scrollTop + (windowHeight - dialogHeight) / 2;
 
     dialog.style.top = `${topPosition}px`;
+}
+
+function addNewOptions(array) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] !== "ZERO") {
+            let option = document.createElement("option");
+            option.value = array[i];
+            option.text = array[i];
+            typeInput.add(option);
+        }
+    }
 }
