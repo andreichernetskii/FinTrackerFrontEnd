fc9646727a3ecd58bcecc4b03f06f306f8fac3e7
orcxus <73879364+andreichernetskii@users.noreply.github.com>
2024-01-19 17:32:27 +0100

    script now edits form in depends on entity for updating

--- src/main/resources/static/scripts/edit-script.js
+++ src/main/resources/static/scripts/edit-script.js
@@ -5,9 +5,9 @@ const typeInput = document.getElementById('type');
 const amountInput = document.getElementById('amount');
 const dateInput = document.getElementById('date');
 const categoryInput = document.getElementById('category');
+const editForm = document.getElementById('edit-form');
 
-
-// by pressing button dialog window will be shown and filled with data from row
+// by pressing editBtn dialog window will be shown and filled with data from row
 document.querySelectorAll('.editBtn').forEach((btn) => {
     btn.addEventListener('click', () => {
 
@@ -38,15 +38,53 @@ document.querySelectorAll('.editBtn').forEach((btn) => {
     });
 });
 
+// edit transaction entity
+document.querySelectorAll('#transaction-editBtn').forEach((btn) => {
+
+    btn.addEventListener('click', () => {
+
+        editForm.setAttribute("action", "/update-transaction");
+        editForm.setAttribute("method", "post");
+        idInput.setAttribute("name", "id");
+        typeInput.setAttribute("name", "financialTransactionType");
+        amountInput.setAttribute("name", "amount");
+        dateInput.setAttribute("name", "date");
+        categoryInput.setAttribute("name", "category");
+
+    });
+});
+
+// edit limit entity
+document.querySelectorAll('.limit-editBtn').forEach((btn) => {
+
+    btn.addEventListener('click', (event) => {
+
+        editForm.setAttribute("action", "/update-limit");
+        editForm.setAttribute("method", "post");
+        idInput.setAttribute("name", "id");
+        typeInput.setAttribute("name", "limitType");
+        amountInput.setAttribute("name", "limitAmount");
+        dateInput.setAttribute("name", "creationDate");
+        categoryInput.setAttribute("name", "category");
+
+    });
+});
+
+
 // by pressing the cancel button dialog window will be closed
+// and reset all changes
 document.getElementById('cancelDialogBtn').addEventListener('click', (event) => {
+
     if (dialog) {
         dialog.classList.add('hidden');
-        typeInput.innerHTML = "";
+        // typeInput.innerHTML = "";
+        // editForm.setAttribute("action", "");
+        location.reload();
     }
 });
 
 function centerDialog() {
+
     const scrollTop = window.scrollY || document.documentElement.scrollTop;
     const dialogHeight = dialog.clientHeight;
     const windowHeight = window.innerHeight;
@@ -57,6 +95,7 @@ function centerDialog() {
 }
 
 function addNewOptions(array) {
+
     for (let i = 0; i < array.length; i++) {
         if (array[i] !== "ZERO") {
             let option = document.createElement("option");
